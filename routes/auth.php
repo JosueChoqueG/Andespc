bfe5c65d95a07fd50eb59ab4eb6e206984c75414
Josue Choque Gomez <110512792+JosueChoqueG@users.noreply.github.com>
2026-01-08 16:04:14 -0500

    Update login

--- routes/auth.php
+++ routes/auth.php
@@ -14,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('guest')->group(function () {
 
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
+    Route::get('login', function () {
+        return redirect('/?login=1');
+    })->name('login');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
